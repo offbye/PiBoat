83879612c34137d0952105a07bafe801c58f08a4
OF1629 <suyubo@qianmi.com>
2015-08-21 19:15:32 +0800

    地图划线

---
+++ androidapp/app/src/main/java/com/qianmi/boat/Constant/GlobalUtil.java
@@ -0,0 +1,10 @@
+package com.qianmi.boat.Constant;
+
+/**
+ * Created by su on 2015/8/21.
+ */
+public class GlobalUtil {
+
+    public static final int MSG_PAINT_CODE = 0x1001;
+
+}
--- androidapp/app/src/main/java/com/qianmi/boat/activity/MapActivity.java
+++ androidapp/app/src/main/java/com/qianmi/boat/activity/MapActivity.java
@@ -2,6 +2,7 @@ package com.qianmi.boat.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
@@ -14,13 +15,22 @@ import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
 import com.baidu.mapapi.map.MyLocationConfiguration;
 import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.map.PolylineOptions;
 import com.baidu.mapapi.model.LatLng;
 import com.qianmi.boat.R;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Created by su on 2015/8/21.
  */
 public class MapActivity extends Activity {
+
+    private String Tag = "MapActivity";
+    private LatLng lastLatLng;
 
     LocationClient mLocClient;
     public MyLocationListenner myListener = new MyLocationListenner();
@@ -30,13 +40,7 @@ public class MapActivity extends Activity {
     MapView mMapView;
     BaiduMap mBaiduMap;
     boolean isFirstLoc = true;// 是否首次定位
-
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
-
+    boolean isFirstCanvas = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +51,41 @@ public class MapActivity extends Activity {
 
         initMapAndLoc();
     }
+
+  /*  //测试在地图上画线
+    public void addCustomElementsDemo(double lat,double lon) {
+        List<LatLng> points = new ArrayList<LatLng>();
+        // 添加折线
+        for(int i=0;i<10;i++){
+            points.add( new LatLng(lat, lon));
+            lat += 0.3;
+            lon += 0.3;
+        }
+        OverlayOptions ooPolyline = new PolylineOptions().width(10)
+                .color(0xAAFF0000).points(points);
+        mBaiduMap.addOverlay(ooPolyline);
+    }*/
+
+    public void addCustomElementsDemo(LatLng latLng1,LatLng latLng2){
+        List<LatLng> points = new ArrayList<LatLng>();
+        points.add(latLng1);
+        points.add(latLng2);
+        OverlayOptions ooPolyline = new PolylineOptions().width(10)
+                .color(0xAAFF0000).points(points);
+        mBaiduMap.addOverlay(ooPolyline);
+
+    }
+
+    public void addCustomEleemntsDemo(LatLng latLng) {
+        if(lastLatLng == null) {
+            lastLatLng = latLng;
+            return;
+        }else {
+            addCustomElementsDemo(lastLatLng, latLng);
+            lastLatLng = latLng;
+        }
+    }
+
 
     /**
      * 定位SDK监听函数
@@ -64,6 +103,14 @@ public class MapActivity extends Activity {
                     .direction(100).latitude(location.getLatitude())
                     .longitude(location.getLongitude()).build();
             mBaiduMap.setMyLocationData(locData);
+            if(isFirstCanvas) {
+                if (location.getLatitude() > 10 && location.getLongitude() > 10) {
+//                    isFirstCanvas = false;
+                   // addCustomElementsDemo((location.getLatitude()), (location.getLongitude()));
+                    addCustomEleemntsDemo(new LatLng(location.getLatitude()+ (5+new Random().nextInt(10))/100f, location.getLongitude()+(5+new Random().nextInt(10))/100f));
+                    Log.v(Tag, "location = " + location.getLatitude() + "  唯独" + location.getLongitude());
+                }
+            }
             if (isFirstLoc) {
                 isFirstLoc = false;
                 LatLng ll = new LatLng(location.getLatitude(),
@@ -97,6 +144,7 @@ public class MapActivity extends Activity {
         mBaiduMap
                 .setMyLocationConfigeration(new MyLocationConfiguration(
                         mCurrentMode, true, mCurrentMarker));
+
     }
 
 
