2f0ecda0f6c7523a9892ec82125eda0f28144cab
OF1629 <suyubo@qianmi.com>
2015-08-21 16:19:22 +0800

    百度地图

---
+++ androidapp/app/src/main/java/com/qianmi/boat/MainApplication.java
@@ -0,0 +1,12 @@
+package com.qianmi.boat;
+
+import android.app.Application;
+
+/**
+ * Created by su on 2015/8/21.
+ */
+public class MainApplication extends Application {
+
+  /*  public LocationClient mLocationClient;
+    public MyLocationListener mMyLocationListener;*/
+}
